1c673481ba9f799007e46d27e21600870e7c75f7
Yingjie Huang <yingjieh58@gmail.com>
2026-07-04 20:31:10 -0700

    added final cpu check speed

---
+++ build-host/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib
+# Build directory: C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/build-host
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[xq_kernel_correctness]=] "C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/build-host/xq_kernel_correctness.exe")
+set_tests_properties([=[xq_kernel_correctness]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/CMakeLists.txt;88;add_test;C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/CMakeLists.txt;0;")
+add_test([=[xq_runtime_correctness]=] "C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/build-host/xq_runtime_correctness.exe")
+set_tests_properties([=[xq_runtime_correctness]=] PROPERTIES  ENVIRONMENT "PATH=C:/msys64/ucrt64/bin\\;C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/build-host/customlib" _BACKTRACE_TRIPLES "C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/CMakeLists.txt;100;add_test;C:/Users/Yingjie Huang/Downloads/mnn-custom-inference-lib/CMakeLists.txt;0;")
+subdirs("customlib")
